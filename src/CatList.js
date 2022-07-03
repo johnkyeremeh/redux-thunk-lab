73b6a22ce7d114cfb6a430805bc099d7257978ba
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-02 17:52:54 -0700

    Completed assignment

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,17 @@
 // write your CatList component here
+import { render } from "enzyme"
+import React from "react"
+
+const CatList = ({catPics})=> {
+
+    const renderCats = catPics.map(cat =>  <div key={cat.id}><img src={cat.url}/></div>)
+
+    return (
+        <div>
+            {renderCats}
+        </div>
+    )
+
+}
+
+export default CatList
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import  catsReducer  from './reducers/catsReducer';
 
+//initialize the store
+import { createStore, applyMiddleware } from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk';
 
-ReactDOM.render(
-    <App />,
+const store = createStore(catsReducer, applyMiddleware(thunk))
+
+ReactDOM.render(<Provider store={store}>
+      <App />
+</Provider>,
     document.getElementById('root')
 );
